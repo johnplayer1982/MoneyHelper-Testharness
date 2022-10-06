ea147124c4d6fef4a923be30d338ece20ebabf31
John Player <johnplayer@IT00468-John-Player.local>
2022-10-06 12:26:16 +0100

    Removes unused imports

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,3 @@
-from django.views.generic import TemplateView
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
